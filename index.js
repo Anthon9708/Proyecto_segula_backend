48e594db21f95935da9a236711500f1965cb3a83
antonio <antoren9708@gmail.com>
2025-02-13 14:02:47 +0100

    añadimos getId y post

--- index.js
+++ index.js
@@ -5,10 +5,35 @@ const Dato = require('./models/Dato');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/datos', async (req, res) => {
   try {
     const datos = await Dato.findAll();
     res.json(datos);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/datos/:id', async (req, res) => {
+  try {
+    const dato = await Dato.findByPk(req.params.id);
+    if (dato) {
+      res.json(dato);
+    } else {
+      res.status(404).send('Dato no encontrado');
+    }
+  } catch (error) {
+    console.error('Error al obtener el dato:', error);
+    res.status(500).send('Error al obtener el dato');
+  }
+});
+
+app.post('/datos', async (req, res) => {
+  try {
+    const nuevoDato = await Dato.create(req.body);
+    res.status(201).json(nuevoDato);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
