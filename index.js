333e9ba3fdd246ca40424a469a014fe14e155ae1
antonio <antoren9708@gmail.com>
2025-02-14 14:01:12 +0100

    conexion a frontend

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const sequelize = require('./config/database');
 const routes = require('./routes/routes');
+const cors = require('cors');
 const conexionBD = require('./bd/ConexionBD');
 
 const app = express();
 const port = 3000;
+app.use(cors({
+    origin: 'http://localhost:5173'
+  }));
 
 app.use(express.json());
 app.use('/', routes);
